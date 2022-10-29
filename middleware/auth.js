602a117fe03fa2c214ea5768edf1eb29f6d8d218
Mohamed Elbashar <mbashar@kashier.io>
2022-10-29 16:59:08 +0200

    change the validation

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,14 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header("x-auth-token");
-  if (!token) return res.status(401).send("Access denied. No token provided.");
+  const token = req.header('x-auth-token');
+  if (!token) return res.status(401).send('Access denied. No token provided.');
   try {
+    // eslint-disable-next-line no-undef
     const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(400).send("Invalid token.");
+    return res.status(400).send('Invalid token.');
   }
 };
